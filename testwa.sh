4725bc781df1cc10cfdcd7350739de93fb55872f
Yayan Maulana <yayan@aimsis.com>
2023-02-05 11:41:45 +0700

    feat: commander

---
+++ testwa.sh
@@ -0,0 +1,4 @@
+curl --location --request POST 'http://goestowedding.com:8000/send-message' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'number=08986182128' \
+--data-urlencode 'message=hahahah hihihih'
